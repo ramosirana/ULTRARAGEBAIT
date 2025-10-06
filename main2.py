accfa74a1e2826f9085cd6488ce378661e25111d
ramosirana <mighellramhose.17@gmail.com>
2025-10-06 22:11:21 +0800

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,13 @@
+from js import document
+
+# stuff
+contact = "Contact Us!"
+number = "+63 979 1234 676"
+email = "idkwhattoputhere@gmail.com"
+soc = "Mikainan! on Facebook and @iforgot on Instagram!"
+
+# displaying said stuff
+document.getElementById("contact").innerText = contact
+document.getElementById("number").innerText = number
+document.getElementById("email").innerText = email
+document.getElementById("soc").innerText = soc
